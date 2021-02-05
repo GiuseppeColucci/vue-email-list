a594ec3f95093113b40b6c004503673731bc6184
giuseppecolucci <giuseppecolucciwork@gmail.com>
2021-02-05 16:07:58 +0100

    finito

--- js/main.js
+++ js/main.js
@@ -1,14 +1,15 @@
 new Vue({
+
     el:'#app',
     data:{
         random:[],
     },
-    memailrandom() {
-        for(const i=0; i<10; i++){
-            const ax=this
+    mounted() {
+        for(let i=0; i<10; i++){
+            const self=this
             axios.get(' https://flynn.boolean.careers/exercises/api/random/mail')
               .then(function(element) {
-                ax.random.push(element.data.response);
+                self.random.push(element.data.response);
                 
               });
             
